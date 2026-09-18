d73564b9011368638f1075a6e6e698f56171ffe8
Akshat-lab886 <wildhive183@gmail.com>
2026-09-18 10:49:00 +0530

    Bump versionCode to 15 (v2.2.0 release)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.bookcon.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "2.1.0"
+        versionCode = 15
+        versionName = "2.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
